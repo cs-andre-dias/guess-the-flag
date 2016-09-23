275519b547ad0c5c75dd3244e9280370b8f3a7b9
Andre Dias <andre.dias@concrete.com.br>
2016-09-23 16:02:48 -0300

    Adiciona arquivos do projeto, projeto ainda em branco, apenas arquivos iniciais

---
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by Andre Dias on 23/09/16.
+//  Copyright © 2016 Andre Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
