619cfbeb05bb13187621a6722bf06b479f681b75
Andre Dias <andre.dias@concrete.com.br>
2016-09-24 20:58:21 -0300

    Deleta a pasta de imagens e coloca em assets, adiciona primeiras flags no primeira tela e adiciona auto-layouts

--- Guess the Flag/Guess the Flag/ViewController.swift
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
